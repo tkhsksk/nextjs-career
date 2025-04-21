f6dd00f041b20d0e922a95e749015aa775759dbc
takahashl <keisuke.takahashi@portia.co.jp>
2025-04-21 14:27:44 +0900

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,7 +11,7 @@ export const metadata: Metadata = {
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)] dark:bg-slate-800">
+    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center h-svh p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)] dark:bg-slate-800">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
         <Image
           className="dark:hidden"
